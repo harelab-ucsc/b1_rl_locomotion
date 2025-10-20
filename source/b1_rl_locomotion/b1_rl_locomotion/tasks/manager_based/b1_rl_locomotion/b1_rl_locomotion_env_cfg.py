2d01cd14bc1b2bdfa39e5611c00d39fa7b89b993
inkyant <80181074+inkyant@users.noreply.github.com>
2025-10-20 02:07:25 +0000

    play less scenes

--- source/b1_rl_locomotion/b1_rl_locomotion/tasks/manager_based/b1_rl_locomotion/b1_rl_locomotion_env_cfg.py
+++ source/b1_rl_locomotion/b1_rl_locomotion/tasks/manager_based/b1_rl_locomotion/b1_rl_locomotion_env_cfg.py
@@ -229,7 +229,7 @@ class B1RlLocomotionEnvCfg_PLAY(B1RlLocomotionEnvCfg):
         # post init of parent
         super().__post_init__()
         # make a smaller scene for play
-        self.scene.num_envs = 50
-        self.scene.env_spacing = 2.5
+        self.scene.num_envs = 5
+        self.scene.env_spacing = 5
         # disable randomization for play
         self.observations.policy.enable_corruption = False
