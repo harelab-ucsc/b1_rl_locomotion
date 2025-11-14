35cd25ecfe5ba42cb387cfc8136a4bfc523a46ec
Oliver Young <oyoung@ucsc.edu>
2025-11-14 09:53:37 -0800

    updated starting pos to correct value

--- source/b1_rl_locomotion/b1_rl_locomotion/tasks/manager_based/b1_rl_locomotion/configs/b1.py
+++ source/b1_rl_locomotion/b1_rl_locomotion/tasks/manager_based/b1_rl_locomotion/configs/b1.py
@@ -54,7 +54,7 @@ B1_CFG = ArticulationCfg(
 
 
     init_state=ArticulationCfg.InitialStateCfg(
-        pos=(0.0, 0.0, 0.46258),
+        pos=(0.0, 0.0, 0.76258),
         # Option 1: Standing Joint pos (values in radians)
         joint_pos={
             "FR_hip_joint": -0.018275,
